bcde4c0b639fe298823d8fddd46a03cc4deff851
MathiasBilloir <mathias.billoir@gmail.com>
2014-11-26 17:26:16 +0100

    add contextManager module

---
+++ tsen.context/contextManager/src/main/java/provider/CommonProvider.java
@@ -0,0 +1,7 @@
+package provider;
+
+/**
+ * Created by mathi_000 on 26/11/2014.
+ */
+public abstract class CommonProvider implements  IProvider {
+}
---
+++ tsen.context/contextManager/src/main/java/provider/IProvider.java
@@ -0,0 +1,7 @@
+package provider;
+
+/**
+ * Created by mathi_000 on 26/11/2014.
+ */
+public interface IProvider {
+}
