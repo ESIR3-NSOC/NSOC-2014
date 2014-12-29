7240fc7061d0245fc18122e8a009fa4d811e3055
Grégory NAIN <gregory.nain@gmail.com>
2014-12-29 11:27:24 +0100

    New versions

---
+++ Model/src/tsen.mm
@@ -0,0 +1,48 @@
+
+class tsen.Room  {
+    @id
+    name : String
+
+    @contained
+    humiditySensor : tsen.Humidity
+    @contained
+    temperatureSensor : tsen.IndoorTemperature
+    @contained
+    valves : tsen.Heater
+    @contained
+    lesson : tsen.Activity[0,*]
+}
+
+class tsen.Humidity  {
+    name : String
+    value : Double
+}
+
+class tsen.IndoorTemperature  {
+    name : String
+    value : Double
+}
+
+class tsen.Heater  {
+    name : String
+    value : Double
+}
+
+class tsen.Activity  {
+    name : String
+    start : Long
+    end : Long
+    @contained
+    members : tsen.User[0,*]
+    @contained
+    targetedTemperature : tsen.Temperature
+}
+
+class tsen.User  {
+    name : String
+    wishState : String
+}
+
+class tsen.Temperature  {
+    value : Double
+}
