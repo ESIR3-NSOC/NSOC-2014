fc3c31cc99e249509be5cef023aab99edaafb2d1
MathiasBilloir <mathias.billoir@gmail.com>
2015-01-23 14:27:55 +0100

    rest classes

--- Restful/src/main/java/rest/dimmableSwitchResource/DimmableSwitchResource.java
+++ Restful/src/main/java/rest/dimmableSwitchResource/DimmableSwitchResource.java
@@ -2,12 +2,12 @@ package rest.dimmableSwitchResource;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.UriInfo;
-import java.awt.*;
 
 /**
  * Created by mathi_000 on 21/01/2015.
@@ -27,6 +27,16 @@ public class DimmableSwitchResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String Ready(){
+        System.out.println("plop ! ");
         return "model is Ready !";
+    }
+
+
+    @Path("vote/{user}")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String setVote(@PathParam("user") String userVote){
+        System.out.println("Vote ! => " + userVote);
+        return "User : " + userVote.split("=")[0] + " vote : " + userVote.split("=")[1];
     }
 }
