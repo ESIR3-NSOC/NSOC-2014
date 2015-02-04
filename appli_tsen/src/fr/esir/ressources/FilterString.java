b8834624b6093ba09b7c9207991ffa83f71f7f0a
Billoir Mathias <mathias.billoir@etudiant.univ-rennes1.fr>
2015-02-04 16:40:06 +0100

    conflict with FilterString

---
+++ appli_tsen/src/fr/esir/ressources/FilterString.java
@@ -0,0 +1,16 @@
+package fr.esir.ressources;
+
+/**
+ * Created by mathi_000 on 04/02/2015.
+ */
+public class FilterString {
+
+    public final static String CONTEXT_EXTRA_DATA =
+            "fr.esir.services.Context_service.EXTRA_DATA";
+
+    public final static String REGULATION_EXTRA_DATA =
+            "fr.esir.services.Regulation_service.EXTRA_DATA";
+
+    public final static String OEP_EXTRA_DATA =
+            "fr.esir.services.Oep_service.EXTRA_DATA";
+}
