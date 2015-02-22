230cf737dae12c25a1e06e7c10492efae821a11e
nicolasvansaingele <n.vansaingele@gmail.com>
2015-02-22 04:14:55 +0100

    solve the multiple asynchtask problem : must be still tested with a not empty db

--- appli_tsen/src/fr/esir/services/Regulation_service.java
+++ appli_tsen/src/fr/esir/services/Regulation_service.java
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.AsyncTask;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
@@ -152,6 +153,7 @@ public class Regulation_service extends Service {
     public boolean initialize() {
         registerReceiver(mServicesUpdateReceiver, makeServicesUpdateIntentFilter());
         regulator = new Regulator();
+        regulator.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
         regulator.setConsigne(18);
         //regulator.execute();
         //regulator.run();
