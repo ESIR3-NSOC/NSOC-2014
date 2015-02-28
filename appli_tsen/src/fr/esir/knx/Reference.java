4610429d8fa71a7f30700cffad15c250f21c9152
nicolasvansaingele <n.vansaingele@gmail.com>
2015-02-28 21:50:18 +0100

    comment references

--- appli_tsen/src/fr/esir/knx/Reference.java
+++ appli_tsen/src/fr/esir/knx/Reference.java
@@ -15,26 +15,27 @@ public class Reference {
     public static final int KNX_PORT = KNXnetIPConnection.IP_PORT;
     public static final int DISCOVERER_TIMEOUT = 30;
     public static final String KNX_ADDRESS = "192.168.1.100";
-    public static final String HOST_ADDRESS = "192.168.1.168";
+    public static final String HOST_ADDRESS = "192.168.1.168"; //android device
     //default values
-    public static final String minTemp = "20";
-    public static final String maxTemp = "25";
+    public static final String minTemp = "20"; //minimal temperature set sent to the regulator
+    public static final String maxTemp = "25"; //minimal temperature set sent to the regulator
     public static final String idRoom = "1005";
     public static final String roomName = "104";
-    public static final int END_OF_DAY_TEMPERATURE = 15;
-    public static final int PAUSE_BTW_LESSONS_TEMPERATURE = 20;
-    public static final long TIME_MIN_TO_CONSIDERE_A_LONG_PAUSE = 35 * 60000;
+    public static final int END_OF_DAY_TEMPERATURE = 15; //temperature set after the end of the last lesson
+    public static final int PAUSE_BTW_LESSONS_TEMPERATURE = 20; //if there a long pause between two lessons, we send this temperature set to the regulator
+    public static final long TIME_MIN_TO_CONSIDERE_A_LONG_PAUSE = 35 * 60000; //if the time between two lessons is more than 35minutes, it's considered as a long pause
     //httpRequest
     public static final int TARGET_PORT = 80;
     public static final String TARGET_HOST = "tsen.uion.fr";
     public static final String TARGET_PROTOCOL = "http";
-    static SharedPreferences pref = ConfigParams.context.getSharedPreferences("APPLI_TSEN", Context.MODE_PRIVATE);
+    public static SharedPreferences pref = ConfigParams.context.getSharedPreferences("APPLI_TSEN", Context.MODE_PRIVATE);
     public static final String TARGET_REQUEST = "/tsen/vote?roomId=" + pref.getString("IDROOM", "1005") + "&delay=60000";
 
+    //calculate time (millisecondes) between the current date and 1am the next day
     public static long timeBefore() {
         Calendar c = Calendar.getInstance();
         c.add(Calendar.DAY_OF_MONTH, 1);
-        c.set(Calendar.HOUR_OF_DAY, 0);
+        c.set(Calendar.HOUR_OF_DAY, 1);
         c.set(Calendar.MINUTE, 0);
         c.set(Calendar.SECOND, 0);
         c.set(Calendar.MILLISECOND, 0);
